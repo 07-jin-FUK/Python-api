71d4f346e3d932f15e14e852b10fe1073c993ab9
07-jin-FUK <dedededeen@icloud.com>
2024-09-13 23:56:21 +0900

    Fridaylast

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask, request, jsonify
 import cv2
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # ルートエンドポイント
 @app.route('/')
